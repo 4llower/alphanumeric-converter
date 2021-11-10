3fa9243540488cb877b76a53b7589a94e98f5437
4llower <4llower@users.noreply.github.com>
2021-11-10 13:57:56 +0000

    deploy: c7a5fa7a250f905bf51c2fc08af31c5550af3797

--- bootstrap.js
+++ bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/alphanumeric_convert_extension_bg.wasm":"0963fff0819e89ce46e4"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/alphanumeric_convert_extension_bg.wasm":"b63efc68502dea4edbcc"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
